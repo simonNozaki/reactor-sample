5a490ea182c0596314ee176ae06ed09f5e0976a5
snozaki <snozaki@netprotections.co.jp>
2020-11-22 17:50:21 +0900

    twitterクライアント追加

--- src/main/kotlin/io/sample/reactive/Main.kt
+++ src/main/kotlin/io/sample/reactive/Main.kt
@@ -2,10 +2,15 @@ package io.sample.reactive
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import net.framework.api.rest.client.SimpleApiClient
+import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
+import twitter4j.Twitter
+import twitter4j.TwitterFactory
 import java.time.Duration
+import java.util.*
 import java.util.logging.Level
 import java.util.logging.Logger
+import kotlin.collections.HashMap
 
 val logger: Logger = Logger.getLogger("reactor-sample")
 
@@ -24,6 +29,22 @@ fun main(args: Array<String>) {
         { it.request(10) }
     )
 
+    val kvs: Mono<KVs> = Mono.just(callRestApi(getJsonUris()["kvs"].orEmpty(), KVs::class.java))
+            .doFinally { logger.info(it.name) }
+            .take(Duration.ofMillis(30000))
+
+    kvs.subscribe(
+        // onSuccess
+        { it.objects.forEach { `object` ->
+                `object`.value.let { println("name : ${it}") }
+            }
+        },
+        // onError
+        { logger.log(Level.SEVERE, "Error occurred when fetching json from remote") },
+        // onComplete
+        { logger.info("Completed fetching data") }
+    )
+
 }
 
 data class KV (
@@ -31,9 +52,14 @@ data class KV (
     var value: String
 )
 
+data class KVs (
+        var objects: List<KV>
+)
+
 fun getJsonUris(): Map<String, String> {
     return HashMap<String, String>().apply {
         put("simple-kv", "https://raw.githubusercontent.com/simonNozaki/jsons/main/simple-kv.json")
+        put("kvs", "https://raw.githubusercontent.com/simonNozaki/jsons/main/kvs.json")
         put("current-price", "https://api.coindesk.com/v1/bpi/currentprice.json")
     }
 }
@@ -48,3 +74,6 @@ fun <T> callRestApi(url: String, clazz: Class<T>): T {
         .get()
         .invoke(clazz)
 }
+
+
+fun getTwitter(): Twitter = TwitterFactory.getSingleton()
